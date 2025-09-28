25fa243ecbb3d1387fe4d2d9d40b740087dee131
Akbar758742 <mdakbar758742@gmail.com>
2025-09-28 12:45:08 +0600

    update

--- Modules/Section/Resources/views/form/terms_and_condition.blade.php
+++ Modules/Section/Resources/views/form/terms_and_condition.blade.php
@@ -1,15 +1,15 @@
-<div class="form-group col-lg-6 ">
-    <label class="label-style-1">{{ ___('label.terms_conditions_title') }} </label>
-    <input type="text" class="form-control input-style-1" name="terms_conditions_title" value="{{ @$section['terms_conditions_title'] }}" required>
-    <input type="hidden" class="form-control input-style-1" name="name[]" value="terms_conditions_title">
+
+
+<div class="col-lg-12 mb-3">
+    <label class="form-label">{{ ___('label.terms_conditions_title') }} </label>
+    <input type="text" placeholder="{{ ___('placeholder.enter_title') }}" class="form-control input-style-1" name="terms_conditions_title" value="{{ old('terms_conditions_title', @$section['terms_conditions_title']) }}">
+    <input type="hidden" class="form-control input-style-1" name="name[]" value="title">
+    @error('title') <p class="text-danger">{{ $message }}</p> @enderror
 </div>
 
-
-
-
-
-<div class="form-group col-lg-6 ">
-    <label class="label-style-1">{{ ___('label.description') }} </label>
-    <input type="text" class="form-control input-style-1" name="description" value="{{ @$section['description'] }}" required>
+<div class="col-lg-12 mb-3">
+    <label class="form-label">{{ ___('label.description') }} </label>
+    <textarea name="description" class="form-control input-style-1 summernote" rows="10">{{ old('description', @$section['description']) }}</textarea>
     <input type="hidden" class="form-control input-style-1" name="name[]" value="description">
+    @error('description') <p class="text-danger">{{ $message }}</p> @enderror
 </div>
